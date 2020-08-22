9ec5b179bea03ef02d7ad3600a8bbbdfc1dc4007
Shubhra <bhatnagar@gmail.com>
2020-08-22 02:05:55 -0400

    Adding code

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# crowdsale
+# CrowFund (Crowdsale) Project and get Puppercoin Token (PUP) as reward
+---
+## Objective
+
+- ### Purpose of this Solidity Contract is to CrowFund (crowdsale) a project and in return the beneficiary gets our custom PUP Token.
+
+## Goal
+- ### Our Goal is to collect 2 ETH and Cap is 50 ETH
+
+## Process
+
+### Connect to Kovan network
+- Open Metamask (or MyCrypto Wallet)
+ 
+### Send Funds to Sales Manager Address: 0x1aFB0466072890Cee6F70213ed4084d147bA4324
+- Click on Send Button
+- Give the above Sales Manager Address
+- And desired amount of ETH ( Exchange for ETH to PUP is 1 to 1)
+ 
+![Sending Eth to Address](Images/SendEthToAddress.png)
+
+#### Crowdsale Completes in 24 weeks from Aug 20th, 2020 and a goal is set to 2 Eth is required. 
+#### The CrowdFunding only allows for refunds or Distribution of Tokens once both the crowdsale is closed and the goal is met.
+
+### How to collect PUP Token after crowdsale is completed:
+- Click on Add Token
+- Click on Custom Token
+- Give the Token Address: 0x447bEc260E9b3F5AB060Db406De716Dce347d550
+- And fill the details like so:
+
+ ![AddingToken](Images/TokenAddition.png)
+
+### The Funds go to the wallet address: 0x59e89683BD9FcAEea66843De01F3a13e842CBD20
+### Contract Id which deployed the contract: 0x9b5a253b0Ec5B437b72CFBC623084d9b3a32515D
+
+### How to Run the Code and play around with it in your localnetwork
+
+0. In `PupperCrowdsale.sol` you can change the close to be `now + 5 minutes` instead of `now + 24 weeks` if you want to see the full cycle of crowdfund close and token distribution
+1. Compile PupperCrowdsale.sol
+2. Deploy PupperCrowdsale.sol with Token name, Token Symbol and Wallet (address where the funds will go) (note the wallet address)
+3. You get multiple functions:
+- Click on goalReached to findout if the crowdsale goal has been reached or not.
+- Click isCampaign open to check if crowdsale campaign is still open or not
+- click on token_ddress Note the token address
+- click on token_sale_address, Note the TokenSale address
+* (Refer to Image below)
+4. In meta mask change to another address.
+5. Click send to the toke_sales_address you got from deployment
+6. And desired amount of ETH ( Refer to the above `Send Fund to Sales Manager Address`)
+7. Once close period is up (in this case now + 5 minutes) click on `FinalizeTheDeal`  (It will cost gas, metamask will open, click on `confirm`)
+  - if you click before the goal is reached or before crowdsale is closed you will get an error with exact error statement.
+8. Once its finalized succesfully you will see the ETHs in the Wallet address you gave during deployment
+9. click on `withdrawToken` (It will cost gas, metamask will open, click on `confirm`)
+10. In metamask connect to the address which sent ETH in get PUP Token
+11. Follow the above section of `how to collect PUP token after crowdsale` with your own token address the you noted after deployment
+12. You should see the PUP Token in your address
+
+![AfterDeployment](Images/ContractDeployment.png)
+
---
+++ Starter-Code/PupperCrowdsale.sol
@@ -0,0 +1,104 @@
+pragma solidity ^0.5.0;
+
+import "./PupperCoin.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
+
+// Inherit the crowdsale contracts
+
+contract PupperCoinSale  is Crowdsale, MintedCrowdsale, CappedCrowdsale, RefundablePostDeliveryCrowdsale{
+
+    // PupperCoin pup_token1 = new PupperCoin("test1234", "TXN", 0);
+    constructor(
+        // Fill in the constructor parameters!
+        uint _rate, // rate in TKNbits
+        address payable _wallet, // sale beneficiary
+        PupperCoin pup_token, // the ArcadeToken itself that the ArcadeTokenSale will work with
+        uint _goal,
+        uint _open,
+        uint _close,
+        uint _cap
+    ) 
+    // Pass the constructor parameters to the crowdsale contracts.
+    Crowdsale(_rate, _wallet, pup_token)  RefundableCrowdsale(_goal) TimedCrowdsale(_open, _close) CappedCrowdsale(_cap)
+    public {
+        require(_goal <= _cap);
+        if(!isOpen()) {
+            finalize();
+        }
+       
+    }
+    
+}
+
+
+contract PupperCoinSaleDeployer {
+
+    // testing: uint fakenow = now;
+    address public token_sale_address;
+    address public token_address;
+    PupperCoinSale puppercoin_sale;
+
+
+    constructor(
+        //Fill in the constructor parameters!
+        string memory name, 
+        string memory symbol, 
+        address payable wallet
+    )
+    
+        public
+    {
+        // create the PupperCoin and keep its address handy
+        PupperCoin token = new PupperCoin(name, symbol, 0);
+        
+        token_address = address(token);
+        uint goal = 2 ether;
+        uint cap = 50 ether;
+        
+        
+        
+        // testing: uint open = fakenow;
+        uint open = now;
+        
+        // testing: uint close = fakenow + 2 minutes;
+        uint close = now + 24 weeks;
+        
+
+        // create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
+        puppercoin_sale = new PupperCoinSale(1, wallet, token, goal, open, close, cap);
+        token_sale_address = address(puppercoin_sale);
+        
+        // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
+        token.addMinter(token_sale_address);
+        token.renounceMinter();
+    }
+    
+    // testing: function fastforward() public {
+    //     fakenow += 10 minutes;
+    // }
+    
+    
+    function goalReached() public view returns (bool) {
+        return  puppercoin_sale.goalReached();
+    }
+    
+     function finalizeTheDeal() public returns(bool){
+        
+        puppercoin_sale.finalize();
+        return puppercoin_sale.finalized();
+    }
+    
+    function isCampaignOpen() public view returns (bool){
+        return puppercoin_sale.isOpen();
+    }
+    
+    function withdrawTokensToBeneficiary() public {
+        puppercoin_sale.withdrawTokens(msg.sender);
+    }
+
+    
+}
